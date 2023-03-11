4286d7b6786c78330fa1fd148c4f3ef815f2e57e
chandra <chandrapenugonda655@gmail.com>
2023-03-12 01:12:32 +0530

    custom middleware

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { UserService } from './user.service';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { CustomException } from 'src/core/filters/custom.exception';
 
 @Controller('user')
 export class UserController {
@@ -8,5 +9,15 @@ export class UserController {
   @Get('/')
   getHello(): string {
     return this.userService.getHello();
+  }
+
+  @Get('/hi')
+  getHi() {
+    throw new CustomException();
+  }
+
+  @Get('/error')
+  getError() {
+    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
   }
 }
